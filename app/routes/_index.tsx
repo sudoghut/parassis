9136a9a1239c2a397e686bc22cedc417d30b8894
Hongsu Wang <sudospace@gmail.com>
2025-02-09 00:32:27 -0500

    creating the basic layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,55 +1,50 @@
 import type { MetaFunction } from "@remix-run/node";
+import { 
+  Menu, 
+  FileUp,
+  LayoutList,
+  Wand,
+  MessageCircle,
+} from "lucide-react";
+
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Parassis Reader" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
-        <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Welcome to <span className="sr-only">Remix</span>
-          </h1>
-          <div className="h-[144px] w-[434px]">
-            <img
-              src="/logo-light.png"
-              alt="Remix"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src="/logo-dark.png"
-              alt="Remix"
-              className="hidden w-full dark:block"
-            />
-          </div>
-        </header>
-        <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-          <p className="leading-6 text-gray-700 dark:text-gray-200">
-            What&apos;s next?
-          </p>
-          <ul>
-            {resources.map(({ href, text, icon }) => (
-              <li key={href}>
-                <a
-                  className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                  href={href}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {icon}
-                  {text}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
+<div className="flex h-screen justify-center">
+  <div className="flex flex-col w-[80%]">
+    <div className="flex flex-row items-center p-4">
+      <div className="flex items-center space-x-4">
+      <div className="text-2xl font-bold">Parassi Reader</div>
+        <FileUp size={24} />
+        <Menu size={24} />
       </div>
     </div>
+    <div className="flex flex-row items-center justify-center p-4">
+      Content
+    </div>
+    <div className="flex flex-row items-center justify-center p-4 w-[10%] border-b self-center">
+    </div>
+    <div className="flex flex-row items-center p-4">
+      <div className="flex items-center space-x-4">
+      <div className="text-2xl font-bold">Assistant</div>
+        <LayoutList size={24} />
+        <Wand size={24} />
+        <MessageCircle size={24} />
+        
+      </div>
+    </div>
+    <div className="flex flex-row items-center justify-center p-4">
+      Anotation
+    </div>
+  </div>
+</div>
   );
 }
 
